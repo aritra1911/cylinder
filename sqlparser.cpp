ee133af6809ed94d139ea2bd8897b42b6b011034
Aritra Sarkar <aritra1911@yahoo.com>
2021-03-20 18:24:22 +0530

    Make `SCHEMA' a subcommand under `CREATE'

--- sqlparser.cpp
+++ sqlparser.cpp
@@ -8,7 +8,8 @@ std::string head(const std::string& s) {
     if (pos != std::string::npos)
         return s.substr(0, pos);
 
-    return "";
+    /* if there are no spaces, then `s' is a single word and hence it's its own head */
+    return s;
 }
 
 std::string tail(const std::string& s) {
@@ -17,17 +18,24 @@ std::string tail(const std::string& s) {
     if (pos != std::string::npos)
         return s.substr(pos + 1);
 
+    /* if there are no spaces, `s' is a single word and nothing follows it */
     return "";
 }
 
 /* TODO: Return an integer based on success / failure */
-Schema parse(const std::string& query) {
-    if (head(query) == "CREATE") {
-        /* TODO: CREATE doesn't just create Schemas */
-        Schema new_schema(tail(query));
-        std::cout << "Schema created.\n";
-        return new_schema;
+Schema parse(const std::string& sql_query) {
+    std::string query = sql_query;  // Create a copy so we don't modify the original one
 
+    if (head(query) == "CREATE") {
+        query = tail(query);
+        if (head(query) == "SCHEMA") {
+            Schema new_schema(tail(query));
+            std::cout << "Schema created.\n";
+            return new_schema;
+        } else {
+            std::cerr << "What's " << head(query) << "? - rest of the line ignored!\n";
+            return Schema("nul");  /* Failure */
+        }
     } else {
         std::cerr << "Couldn't parse query\n";
         return Schema("nul");  /* Failure */
